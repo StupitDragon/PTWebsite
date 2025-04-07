caa321ccbed68956eb8ad84f5a65bb4fb8ba595d
Gitee_HLW <13270082+gitee-hlw@user.noreply.gitee.com>
2025-04-07 19:18:04 +0800

    try again

---
+++ PTWebsite/src/main/java/com/intelligentclass/ptwebsite/PtWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.intelligentclass.ptwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtWebsiteApplication.class, args);
+    }
+
+}
---
+++ PTWebsite/src/test/java/com/intelligentclass/ptwebsite/PtWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligentclass.ptwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
